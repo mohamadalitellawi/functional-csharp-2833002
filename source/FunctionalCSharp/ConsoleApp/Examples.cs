96f82cc3ce33b3eeb7d540ab3c45d4450dc621e5
mohamad ali <mohamadalitellawi@gmail.com>
2020-09-18 09:25:24 +0300

    sperate extension methode

--- source/FunctionalCSharp/ConsoleApp/Examples.cs
+++ source/FunctionalCSharp/ConsoleApp/Examples.cs
@@ -43,12 +43,4 @@ namespace ConsoleApp
 		} 
 		#endregion
 	}
-
-	public static class Extensions
-	{
-		public static Func<T, TReturn2> Compose<T, TReturn1, TReturn2>(this Func<TReturn1, TReturn2> func1, Func<T, TReturn1> func2)
-		{
-			return x => func1(func2(x));
-		}
-	}
 }
---
+++ source/FunctionalCSharp/ConsoleApp/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+	public static class Extensions
+	{
+		public static Func<T, TReturn2> Compose<T, TReturn1, TReturn2>(this Func<TReturn1, TReturn2> func1, Func<T, TReturn1> func2)
+		{
+			return x => func1(func2(x));
+		}
+	}
+}
